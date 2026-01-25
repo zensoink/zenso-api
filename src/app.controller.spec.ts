62fa73b28d5a187a0cc99e58911cece47c9cf55a
Konrad Stępień <kord.stp@gmail.com>
2026-01-25 20:59:06 +0100

    test: update tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello you fucking count!"', () => {
+      expect(appController.getHello()).toBe('Hello you fucking count!');
     });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let mockPrismaService: jest.Mocked<PrismaService>;
 
   beforeEach(async () => {
+    const mockUserCreate = jest.fn();
+
+    mockPrismaService = {
+      user: {
+        create: mockUserCreate,
+      },
+    } as unknown as jest.Mocked<PrismaService>;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -15,5 +32,30 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const createUserDto: CreateUserDTO = {
+      name: 'John Doe',
+      email: 'john@example.com',
+    };
+
+    const expectedUser = {
+      id: 1,
+      name: createUserDto.name,
+      email: createUserDto.email,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const mockCreate = jest.fn().mockResolvedValue(expectedUser);
+    mockPrismaService.user.create = mockCreate;
+
+    const result = await service.createUser(createUserDto);
+
+    expect(mockCreate).toHaveBeenCalledWith({
+      data: createUserDto,
+    });
+    expect(result).toEqual(expectedUser);
   });
 });
