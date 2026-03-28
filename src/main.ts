c68de1b479ed7af42e5af55740355004ce3bdea9
Konrad Stępień <kord.stp@gmail.com>
2026-03-28 08:40:22 +0100

    refactor(docker): optimize Dockerfile, entrypoint, and configurations (#3)
    * refactor(docker): optimize Dockerfile, entrypoint, and configurations
    
    * chore(tsconfig): adjust include/exclude paths and resolve bootstrap promise

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableShutdownHooks();
+
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
-void bootstrap().then();
+void bootstrap().then(() => {});
