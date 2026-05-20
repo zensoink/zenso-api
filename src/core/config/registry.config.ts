0ca9e9186ffb515a8ca432ba6a1ec7f9ab600dc7
Konrad Stępień <kord.stp@gmail.com>
2026-05-20 22:23:23 +0200

    feat(plugins): add mock registry for development and testing

--- src/core/config/registry.config.ts
+++ src/core/config/registry.config.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('registry', () => ({
   url: process.env.REGISTRY_URL || 'https://registry.zenso.ink',
+  mockEnabled: process.env.MOCK_REGISTRY === 'true',
 }));
